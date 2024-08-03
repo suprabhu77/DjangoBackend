07f11d0605378371cb8621b68037f1d5cba86c6e
sumantha <sumanthprabhu2000@gmail.com>
2024-08-03 14:59:56 +0530

    Updated the Migrations automatically

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class Note(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    post = models.CharField(max_length=255, default="Inital Comment......")
 
     class meta:
         db_table = "note"
--- build_files.sh
+++ build_files.sh
@@ -2,4 +2,5 @@
 pip install -r requirements.txt
 
 # make migrations
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate
