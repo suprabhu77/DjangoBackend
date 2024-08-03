86056fe4a8aa3e82f3ddbaf588d53629fc140da6
sumantha <sumanthprabhu2000@gmail.com>
2024-08-03 11:44:37 +0530

    Upgraded the migration files

--- api/models.py
+++ api/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Note(models.Model):
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
     description = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
