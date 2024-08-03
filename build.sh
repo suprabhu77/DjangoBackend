03dea8c570325604cfe7c1e5782faf9465fd2306
sumantha <sumanthprabhu2000@gmail.com>
2024-08-03 16:00:04 +0530

    One last time for Migrations

--- build.sh
+++ build.sh
@@ -1,15 +1,7 @@
-#!/bin/bash
-
-# Update package lists
-apt-get update
-
-# Install Python and pip
-apt-get install -y python3.9 python3.9-pip
-
 # Install dependencies
 pip3 install -r requirements.txt
 
 # make migrations
 echo "Running MIgrations...."
-python3.9 manage.py makemigrations
+# python3.9 manage.py makemigrations
 python3.9 manage.py migrate
